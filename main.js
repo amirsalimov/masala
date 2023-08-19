3a95fb75de272a0b86fe9997b5e8d8631a14eb38
amirsalimov <salimovamirxon27@gmail.com>
2023-08-19 16:19:12 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,479 @@
+// let players = [
+// {name:'messi',
+// age:37,
+// color:white;
+// isplay:true,
+// married:true,
+// adress:barcelona,
+// number:10,
+// money:10000$,
+// iscar:true,
+// ischildren:true,}
+
+// {
+//     name:'m.salah',
+//     age:31,
+//     color:wheat color;
+//     isplay:true,
+//     married:true,
+//     adress:liverpool,
+//     number:10,
+//     money:20000$,
+//     iscar:true,
+//     ischildren:true,  
+// }
+// {
+//     name:'Neymar',
+//     age: '30',
+//     isplay: true,
+//     number:10,
+//     wincup:35,
+//     money:55000000,
+//     price:7500000,
+//     marryed:null,
+//     address: 'Barselona',
+// }
+// {
+//     name:'ronaldinho',
+// age:39,
+// color:black;
+// isplay:false,
+// married:true,
+// adress:barcelona,
+// number:80,
+// money:100000$,
+// iscar:true,
+// ischildren:true,}
+// {
+//     name:'ronaldo',
+// age:38,
+// color:white;
+// isplay:true,
+// married:true,
+// adress:real madrird,
+// number:7,
+// money:100000$,
+// iscar:true,
+// ischildren:true,}
+
+
+
+// ]
+// console.log(players);
+
+// let cars = {
+// name:Mercedes benz;
+// age;97;
+// color:white;
+// car name; cls63(banan)
+// gasoline:true;
+// hatch:true;
+// comfort:true;
+// isbest:true(best of the best;);
+
+// }
+
+// N1
+//      if else
+// var num=6;
+// if(num>0) {
+//     console.log("musbat");
+// } else{
+//     console.log("manfiy");
+// }
+
+// N2
+// var num=8;
+// if(num!==3){
+//     console.log("toq son");
+// }else{
+//     console.log("topilmadi");
+// }
+
+// N3
+// var num=7
+// if(num%2==0){
+//     console.log("juft son");
+// }else{
+//     console.log("juft emas");
+// }
+
+//  N8
+// var num=1
+// if(num%2!==0){
+//     console.log("toq son");
+// }else{
+//     console.log("toq son emas");
+// }
+// N9
+//    var res = a;
+//    var res = b;
+// if(resM-s a > 0 && b>0) {
+//     console.log("rostlk");
+// }else{
+//     console.log("emas");
+// }
+
+//  N10
+
+    // var num = 3;
+    // var num = 3;
+     // var num = 4;
+// if(num a>b , b>c){
+//     console.log("rostlik");
+// }else{
+//     console.log("emas");
+// }
+//N11
+// var x=4;
+// var y=5;
+// if(x>0 && y>0){
+//     console.log("musbat");
+// }else{
+// console.log("manfiy");
+// }
+//N12
+//  var age = 22;
+//  if(age>22){
+//     console.log("sizga kredit beriladi");
+//  }else{
+//     console.log("sizning yoshingiz yetmaydi");
+//  }
+
+// HOISTING              
+// N1
+// console.log(res);
+// var res = 1;
+// N2
+// console.log(res);
+// var res = 2;
+// N3
+// console.log(res);
+// var res = 3;
+// N4
+// console.log(res);
+// var res = 4;
+// N5
+// console.log(res);
+// var res = 5;
+// N6
+// console.log(res);
+// var res = 6;
+// N7
+// console.log(res);
+// var res = 7;
+// N8
+// console.log(res);
+// var res = 8;
+// N9
+// console.log(res);
+// var res = 9;
+// N10
+// console.log(res);
+// var res = 10;
+// N11
+// console.log(res);
+// var res = 11;
+// N12
+// console.log(res);
+// var res = 12;
+// N13
+// console.log(res);
+// var res = 13;
+// N14
+// console.log(res);
+// var res = 14;
+// N15
+// console.log(res);
+// var res = 15;
+// N16
+// console.log(res);
+// var res = 16;
+// N17
+// console.log(res);
+// var res = 17;
+// N18
+// console.log(res);
+// var res = 18;
+// N19
+// console.log(res);
+// var res = 19;
+// N20
+// console.log(res);
+// var res = 20;
+// N21
+// console.log(res);
+// var res = 21;
+// N22
+// console.log(res);
+// var res =22;
+// N23
+// console.log(res);
+// var res = 23;
+// N24
+// console.log(res);
+// var res = 24;
+// N25
+// console.log(res);
+// var res = 25;
+// N26
+// console.log(res);
+// var res = 26;
+// N27
+// console.log(res);
+// var res = 27;
+// N28
+// console.log(res);
+// var res = 28;
+// N29
+// console.log(res);
+// var res = 29;
+// N30
+// console.log(res);
+// var res = 31;
+// N31
+// console.log(res);
+// var res = 31;
+// N32
+// console.log(res);
+// var res = 32;
+// N33
+// console.log(res);
+// var res = 33;
+// N34
+// console.log(res);
+// var res = 34;
+// N35
+// console.log(res);
+// var res = 35;
+// N36
+// console.log(res);
+// var res = 36;
+// N37
+// console.log(res);
+// var res = 37;
+// N38
+// console.log(res);
+// var res = 38;
+// N39
+// console.log(res);
+// var res = 39;
+// N40
+// console.log(res);
+// var res = 40;
+// N41
+// console.log(res);
+// var res = 41;
+// N42
+// console.log(res);
+// var res = 42;
+// N43
+// console.log(res);
+// var res = 43;
+// N44
+// console.log(res);
+// var res = 44;
+// N45
+// console.log(res);
+// var res = 45;
+// N46
+// console.log(res);
+// var res = 46;
+// N47
+// console.log(res);
+// var res = 47;
+// N48
+// console.log(res);
+// var res = 48;
+// N49
+// console.log(res);
+// var res = 49;
+// N50
+// console.log(res);
+// var res = 50;
+
+//FUNCTION
+//N1
+// function checkage(age){
+//     if(age>18){
+//         console.log("sen prava olasan");
+//     }else{
+//         console.log("sen prava ololmaysan");
+//     }
+// }
+// checkage(age)
+//N2
+// function checkage(age){
+//     if(age>16){
+//         console.log("senga pasprot beriladi");
+//     }else{
+//         console.log("sening yoshing yetmaydi");
+//     }
+// }
+// checkage(age)
+
+//N3
+//  function check a,b(a,b){
+//     if(x>0 && y>0){
+//         console.log("musbat");
+//     }else{
+//         console.log("manfiy");
+//     }
+//  }
+// check(a,b)
+//N4
+// function checknum(num){
+//     if(num%2!==0){
+//     console.log("toq son");
+// }else{
+//     console.log("toq son emas");
+// }
+// }
+//checknum(num)
+//N5
+// function checknum(num){
+//   if(num%2==0){
+//     console.log("juft son");
+// }else{
+//     console.log("juft emas");
+// }
+// }
+//checknum(num)
+//N6
+// function checknum(num){
+// if(num!==3){
+//     console.log("toq son");
+// }else{
+//     console.log("topilmadi");
+// }
+// }
+//checknum(num)
+//N7
+// function checknum(num){
+// if(num>0) {
+//     console.log("musbat");
+// } else{
+//     console.log("manfiy");
+// }
+// }
+// checknum(num)
+//N8
+// function checknum(num){
+//     if(num a>b , b>c){
+//     console.log("rostlik");
+// }else{
+//     console.log("emas");
+// }
+// }
+// checknum(num)
+//N9
+// function checkage (age){
+//     if(age>18){
+//         console.log("Siz prezidentlika nomzod saylayolasiz");
+//     }else{
+//         console.log("Sizning yoshingiz togri kelmaydi");
+//     }
+// }
+// checkage (num)
+//N10
+// function checkage (num){
+//     if(num>13){
+//     console.log("Oyinda qatnasha olasiz");
+//     }else{
+//         console.log("Oyinda qatnashishga yoshigmiz togr kelmaydi");
+//     }
+// }
+// checkage (num)
+//N11
+// function testNum(a) {
+//     let result;
+//     if (a > 0) {
+//       result = 'positive';
+//     } else {
+//       result = 'NOT positive';
+//     }
+//     return result;
+//   }
+  
+//   console.log(testNum(-5));
+//N12
+// function checkValue(a, b) {
+//     if (a === 1)
+//       if (b === 2)
+//         console.log("a is 1 and b is 2");
+//     else
+//       console.log("a is not 1");
+//   }
+//checkValue(a,b)
+
+
+//   N13
+// function repr(x,y){
+//     return x*y;
+// }
+// console.log(repr(2,3));
+
+
+//   N14
+// function num(x,y){
+//     return x**y;
+// }
+// console.log(num(3,3));
+
+
+//   N15
+// function num(x,y){
+//     return x+y;
+// }
+// console.log(num(1,3));
+
+
+//   16
+// function num(x,y,z){
+//     return x+y+z;
+// }
+// console.log(num(1,2,3));
+
+
+
+//DATATYPES
+//N1
+// let students = ['Gani', 'Ali']
+// console.log(students);
+//N2
+// let date = '25.04.2001'
+// console.log(date);
+// N3
+// let students = {'Gani', age:27, course:5};
+// console.log(students);
+// N4
+// let car = ['Gentra']
+// console.log(car);
+// N5
+// let noutbooks = {'mac', 'hp', 'asus'};
+// console.log(noutbooks);
+// N6
+// let phone = {'iphone', 'samsung', 'redmi', 'honor'};
+// console.log(phone);
+// N7
+// let district = {'yunsobod', 'yashnabod', 'chilonzor'};
+// console.log(district);
+//N8
+// num = 5;
+// let res = Math.floor(num);
+// console.log(res);
+//N9
+// num = 7.48;
+// let res = Math.floor(num)
+// console.log(res);
+// //N10
+// num = 9.549;
+// let res = Math.floor(num)
+// console.log(res);
+
+//   N11
+// let code = 'Hello world';
+// console.log(code);
+//N14
+// let color = 'white';
+// console.log(color)
